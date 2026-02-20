44536ea36354b26458befb18540ff0e94fcbf8fa
Sanjeev MS <sanjeevms@Sanjeevs-MacBook.local>
2026-02-20 11:37:39 +0530

    test: configure jest rtl unit testing framework with github actions ci pipeline (Assignment 2.45)

---
+++ bloodos/jest.setup.ts
@@ -0,0 +1,4 @@
+
+// Import jest-dom to automatically add custom matchers for asserting on DOM nodes.
+// Allows using expect(element).toBeInTheDocument() or expecting element attributes
+import '@testing-library/jest-dom';
